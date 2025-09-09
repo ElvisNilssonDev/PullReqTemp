2c68599ffd6caed3649ca0a74a3ae100d6a2dfb3
ElvisNilssonDev <Nilssonelvis@gmail.com>
2025-09-09 14:19:18 +0200

    new

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, we are getting somewhere!");
+            Console.WriteLine("Hello, we are getting ssomewhere!");
             Console.ReadLine();
         }
     }
