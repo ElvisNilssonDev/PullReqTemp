2e086fc0c77f796cfa8c449829a8aae9bb5db168
ElvisNilssonDev <Nilssonelvis@gmail.com>
2025-09-09 13:21:52 +0200

    I hvae done something

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World its mmme agin!");
+            Console.WriteLine("Hello, World its mmagin!");
             Console.ReadLine();
         }
     }
