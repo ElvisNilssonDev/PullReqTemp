f494ee9629d8536b992949fc019064d9c6978893
ElvisNilssonDev <Nilssonelvis@gmail.com>
2025-09-09 13:59:32 +0200

    added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World itsss xmzzzzmagin!");
+            Console.WriteLine("Hello, we are getting somewhere!");
             Console.ReadLine();
         }
     }
