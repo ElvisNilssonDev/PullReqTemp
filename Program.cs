eff584df01e800f7267087f019dd8dfdbc46dcbc
ElvisNilssonDev <Nilssonelvis@gmail.com>
2025-09-09 13:20:59 +0200

    xfasf

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World its me agin!");
+            Console.WriteLine("Hello, World its mmme agin!");
             Console.ReadLine();
         }
     }
